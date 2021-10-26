d82dca800975b14c153ba7249f406529a45ed685
deems <tdeems@ya.ru>
2021-10-26 19:01:40 +0300

    отключил /news

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ logging.basicConfig(level=logging.INFO)
 
 def register_handlers():
     dp.register_message_handler(bot_service.on_new_user, content_types=['new_chat_members'])
-    dp.register_message_handler(bot_service.welcome, commands=['start', 'help'])
-    dp.register_message_handler(news_service.top_news, commands=['news'])
+    #dp.register_message_handler(bot_service.welcome, commands=['start', 'help'])
+    #dp.register_message_handler(news_service.top_news, commands=['news'])
     dp.register_message_handler(score_service.get_top, commands=['cats_top'])
     dp.register_message_handler(score_service.update_score, regexp='спасибо|\+')
     # dp.register_message_handler(bot_service.echo)
