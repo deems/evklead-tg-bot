66f3290afe39112f9c0b00954049888393f4328a
deems <tdeems@ya.ru>
2022-06-14 09:23:06 +0300

    убрал кнопки лайков

--- services/news_service/news_service.py
+++ services/news_service/news_service.py
@@ -8,7 +8,6 @@ from aiogram import types
 import settings
 from bot import bot
 from redis_pool import redis
-from services.likes_service.like_service import like_service
 from services.locales_service.locales_service import locales_service
 from services.news_service.data.news_item import NewsItem
 
@@ -50,20 +49,18 @@ class NewsService:
     async def top_news(self, message: types.Message):
         news = await self._process_news()
         if news:
-            buttons = await like_service.get_likes_buttons()
+            #buttons = await like_service.get_likes_buttons()
             for item in news:
                 await bot.send_message(message.chat.id, item.text,
-                                       parse_mode=types.ParseMode.HTML,
-                                       reply_markup=buttons)
+                                       parse_mode=types.ParseMode.HTML)
 
     async def send_top_news(self):
         news = await self._process_news()
         if news:
-            buttons = await like_service.get_likes_buttons()
+            #buttons = await like_service.get_likes_buttons()
             for item in news:
                 await bot.send_message(settings.CHAT_FOR_NEWS, item.text,
-                                       parse_mode=types.ParseMode.HTML,
-                                       reply_markup=buttons)
+                                       parse_mode=types.ParseMode.HTML)
 
 
 news_service = NewsService()
